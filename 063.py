f6ac326d016fae0d2771f07396da4f9270c25e97
estherxp <143227795+estherxp@users.noreply.github.com>
2023-12-02 09:35:06 -0300

    063.py
    Nesta programa o usuário digita uma expressão qualquer que use parênteses. E o aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.

--- 063.py
+++ 063.py
@@ -1,3 +1,4 @@
+# Desafio:
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta
 def verf_parentes(expressao):
@@ -19,10 +20,6 @@ if verf_parentes(usuario):
 else:
     print('Os parênteses não estão corretos!')
 
-# quando encontra um parentese aberto,
-# ele joga para a pilha ate encontrar um parentese fechado apagando o ultimo elemento
-# quando o looop terminar e a pilha estiver vazia significa que esta correto o codigo devera retornar true
-
-
-
-
+# quando encontra um parentese aberto, ele joga para a pilha ate encontrar um parentese fechado,
+# "apagando" o ultimo elemento quando o looop terminar e a pilha estiver vazia significa que esta
+# correto o codigo devera retornar true.
