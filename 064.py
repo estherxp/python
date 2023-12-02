e8e2a866013e571d6001d636a660e86a328e5505
estherxp <143227795+estherxp@users.noreply.github.com>
2023-12-02 09:51:34 -0300

    064.py

--- 064.py
+++ 064.py
@@ -1,2 +1,15 @@
 # Crie um programa onde o usuário possa digitar sete letras,
 # e cadastre em uma única lista, que mantenha separado as consoantes das vogais
+
+lista = [[], []] # lista dentro de outra lista
+
+for i in range(0, 7):
+    letra = input('Digite uma Letras: ')
+    if letra in 'aeiou':
+        lista[0].append(letra)
+    else:
+        lista[1].append(letra)
+
+print(f'Vogais: {lista[0]}'
+      f'\n Consoantes: {lista[1]}'
+      f'\n A lista completa é {lista}')
